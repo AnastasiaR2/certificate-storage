3b639541fd6d7e2b958a885fcc151a0b066d91e8
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2024-03-13 12:42:01 +0200

    Add main route

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { AppRoute } from '@/common/enums/enums.js';
+import { Main } from '@/pages/main/main.jsx';
+
 const App: React.FC = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path={AppRoute.ROOT} element={<Main />} />
+    </Routes>
+  );
 };
 
 export { App };
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,5 @@
+const Main: React.FC = () => {
+  return <></>;
+};
+
+export { Main };
